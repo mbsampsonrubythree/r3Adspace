209df58056c20a98b54e02e42470cbe915438f4a
Matt Sampson <matthew@therubythree.com>
2018-03-13 16:49:26 -0700

    Fixed migration error with Ads model

---
+++ app/views/ads/_ad.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! ad, :id, :title, :description, :image, :created_at, :updated_at
+json.url ad_url(ad, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180313233831) do
+
+  create_table "ads", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
